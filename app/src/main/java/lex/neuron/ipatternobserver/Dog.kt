5667800641a4e09b946f6b2c010cf3f3e9ae784c
open032 <37606237+open032@users.noreply.github.com>
2021-01-10 11:57:50 +0700

    Update Dog.kt
    change *

--- app/src/main/java/lex/neuron/ipatternobserver/Dog.kt
+++ app/src/main/java/lex/neuron/ipatternobserver/Dog.kt
@@ -6,10 +6,10 @@ data class Dog(val name: String) : PenguinInDanger {
     private val TAG = "TAG"
 
     /**
-    The dog always receives a signal that the penguin is in danger.
-    He cannot understand the coordinates,
-    so he just runs in search of a penguin on her back
-     **/
+     * The dog always receives a signal that the penguin is in danger.
+     * He cannot understand the coordinates,
+     * so he just runs in search of a penguin on her back
+     */
     override fun getCoordinates(x: Int, y: Int) {
         Log.d(TAG, "Шарик искать")
     }
